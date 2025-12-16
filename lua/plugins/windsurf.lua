cdd9ddc43f8288598cee80a2ea13fcd6b66f55bc
mikothedog <gchaimowicz.dev@gmail.com>
2025-12-16 22:49:11 +0200

    disable avante

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,14 +1,14 @@
-return {
-	"Exafunction/windsurf.vim",
-	event = "InsertEnter", -- Lazy-load when entering insert mode
-	config = function()
-		vim.g.codeium_disable_bindings = 1
-
-		-- Define your own mappings (example)
-		vim.keymap.set('i', '<S-Tab>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-		vim.keymap.set('i', '<C-;>', function() return vim.fn end, { expr = true, silent = true })
-		vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-			{ expr = true, silent = true })
-		vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-	end,
-}
+-- return {
+-- 	"Exafunction/windsurf.vim",
+-- 	event = "InsertEnter", -- Lazy-load when entering insert mode
+-- 	config = function()
+-- 		vim.g.codeium_disable_bindings = 1
+--
+-- 		-- Define your own mappings (example)
+-- 		vim.keymap.set('i', '<S-Tab>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+-- 		vim.keymap.set('i', '<C-;>', function() return vim.fn end, { expr = true, silent = true })
+-- 		vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+-- 			{ expr = true, silent = true })
+-- 		vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+-- 	end,
+-- }
